affe7c56e1d636cd138ef7d08ed72e34c64c8167
Richard Nunes <richardnunes.gm@gmail.com>
2019-03-02 18:12:53 +0000

    Adding styling to droppables and dragables.

--- src/column.js
+++ src/column.js
@@ -13,6 +13,7 @@ const Title = styled.h3`
 `;
 const TaskList = styled.div`
     padding: 8px;
+    background: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
 `;
 
 export default class Column extends React.Component {
@@ -21,10 +22,11 @@ export default class Column extends React.Component {
             <Container>
                 <Title>{this.props.column.title}</Title>
                 <Droppable droppableId={this.props.column.id}>
-                    {provided => (
+                    {(provided, snapshot) => (
                         <TaskList
                             ref={provided.innerRef}
                             {...provided.droppableProps}
+                            isDraggingOver={snapshot.isDraggingOver}
                         >
                             {this.props.tasks.map((task, index) => (
                                 <Task key={task.id} task={task} index={index} />))
